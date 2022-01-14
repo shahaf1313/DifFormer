8a48af1dceb969e23d743c0a5980c220668cf68b
Shahaf <shahaf1313@gmail.com>
2022-01-14 17:08:00 +0200

    local commit

--- scripts/image_train.py
+++ scripts/image_train.py
@@ -3,22 +3,17 @@ Train a diffusion model on images.
 """
 
 import argparse
-
-from guided_diffusion import dist_util, logger
-from guided_diffusion.image_datasets import load_data
-from guided_diffusion.resample import create_named_schedule_sampler
-from guided_diffusion.script_util import (
-    model_and_diffusion_defaults,
-    create_model_and_diffusion,
-    args_to_dict,
-    add_dict_to_argparser,
-)
-from guided_diffusion.train_util import TrainLoop
-
+import os
 
 def main():
     args = create_argparser().parse_args()
-
+    # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    # os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpus)[1:-1].strip(' ').replace(" ", "")
+    from guided_diffusion import dist_util, logger
+    from guided_diffusion.image_datasets import load_data
+    from guided_diffusion.resample import create_named_schedule_sampler
+    from guided_diffusion.script_util import create_model_and_diffusion
+    from guided_diffusion.train_util import TrainLoop
     dist_util.setup_dist()
     logger.configure()
 
@@ -56,6 +51,96 @@ def main():
         lr_anneal_steps=args.lr_anneal_steps,
     ).run_loop()
 
+def diffusion_defaults():
+    """
+    Defaults for image and classifier training.
+    """
+    return dict(
+        learn_sigma=False,
+        diffusion_steps=1000,
+        noise_schedule="linear",
+        timestep_respacing="",
+        use_kl=False,
+        predict_xstart=False,
+        rescale_timesteps=False,
+        rescale_learned_sigmas=False,
+    )
+
+
+def classifier_defaults():
+    """
+    Defaults for classifier models.
+    """
+    return dict(
+        image_size=64,
+        classifier_use_fp16=False,
+        classifier_width=128,
+        classifier_depth=2,
+        classifier_attention_resolutions="32,16,8",  # 16
+        classifier_use_scale_shift_norm=True,  # False
+        classifier_resblock_updown=True,  # False
+        classifier_pool="attention",
+    )
+
+
+def model_and_diffusion_defaults():
+    """
+    Defaults for image training.
+    """
+    res = dict(
+        image_size=64,
+        num_channels=128,
+        num_res_blocks=2,
+        num_heads=4,
+        num_heads_upsample=-1,
+        num_head_channels=-1,
+        attention_resolutions="16,8",
+        channel_mult="",
+        dropout=0.0,
+        class_cond=False,
+        use_checkpoint=False,
+        use_scale_shift_norm=True,
+        resblock_updown=False,
+        use_fp16=False,
+        use_new_attention_order=False,
+    )
+    res.update(diffusion_defaults())
+    return res
+
+
+def classifier_and_diffusion_defaults():
+    res = classifier_defaults()
+    res.update(diffusion_defaults())
+    return res
+
+
+
+def add_dict_to_argparser(parser, default_dict):
+    for k, v in default_dict.items():
+        v_type = type(v)
+        if v is None:
+            v_type = str
+        elif isinstance(v, bool):
+            v_type = str2bool
+        parser.add_argument(f"--{k}", default=v, type=v_type)
+
+
+def args_to_dict(args, keys):
+    return {k: getattr(args, k) for k in keys}
+
+
+def str2bool(v):
+    """
+    https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+    """
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("boolean value expected")
 
 def create_argparser():
     defaults = dict(
@@ -71,11 +156,12 @@ def create_argparser():
         save_interval=10000,
         resume_checkpoint="",
         use_fp16=False,
-        fp16_scale_growth=1e-3,
+        fp16_scale_growth=1e-3
     )
     defaults.update(model_and_diffusion_defaults())
     parser = argparse.ArgumentParser()
     add_dict_to_argparser(parser, defaults)
+    parser.add_argument("--gpus", type=int, nargs='+', help="String that contains available GPUs to use", default=[0])
     return parser
 
 
